34c4d713a05794b662ef67535279dfdfbd6ded88
faycalEdderi <faycmx@live.fr>
2025-10-16 11:00:42 +0200

    implementing easiest

---
+++ newDQN/DQNAgent.py
@@ -0,0 +1,51 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+import numpy as np
+import random
+from collections import deque
+
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95
+        self.epsilon = 1.0 
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(learning_rate=self.learning_rate))
+        return model
+    
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        q_values = self.model.predict(state)
+        return np.argmax(q_values[0])
+    
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+    
+    def replay(self, batch_size):
+        minibach = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibach:
+            target = reward
+            if not done:
+                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+    
+    def save(self, name):
+        self.model.save(name)
---
+++ newDQN/main.py
@@ -0,0 +1,35 @@
+import os
+import DQNAgent as dqn
+import TradingEnv as te
+import numpy as np 
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+
+os.makedirs('models', exist_ok=True)
+
+data = pd.read_csv('dataset/top10_stocks_2025.csv')
+prices = data['Close'].values.reshape(-1, 1)
+
+scaler = MinMaxScaler()
+prices = scaler.fit_transform(prices).flatten()
+env = te.TradingEnv(prices)
+agent = dqn.DQNAgent(state_size=3, action_size=3)
+
+for episode in range(10):
+    state = env.reset()
+    state = np.reshape(state, [1, 3])
+    total_reward = 0
+    for time in range(len(prices)-1):
+        action = agent.act(state)
+        next_state, reward, done, _ = env.step(action)
+        total_reward += reward
+        next_state = np.reshape(next_state, [1, 3])
+        agent.remember(state, action, reward, next_state, done)
+        state = next_state
+        if done:
+            print(f"Episode: {episode+1}/10, Total Reward: {total_reward}")
+            break
+    if len(agent.memory) > 32:
+        agent.replay(32)
+agent.save("models/dqn_trading_model.h5")
